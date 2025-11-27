9505163b83c3abd41248e2882755a1c5413e6b28
Dean Howe <hideanhowe@gmail.com>
2025-11-27 13:35:39 +0000

    setup

--- index.js
+++ index.js
@@ -4,3 +4,22 @@
 // 3 with "Fizz".
 // 5 with "Buzz".
 // 3 and 5 with "FizzBuzz".
+
+function fizzBuzz(n) {
+  console.log(n);
+  let counter = n;
+  if (counter > 0) {
+    counter--;
+    if (counter % 5 && counter % 3) {
+      counter = 'FizzBuzz';
+    } else if (counter % 5) {
+      counter = 'Buzz';
+    } else if (counter % 3) {
+      counter = 'Fizz';
+    }
+    console.log(counter);
+  }
+  return counter;
+
+  // return n;
+}
